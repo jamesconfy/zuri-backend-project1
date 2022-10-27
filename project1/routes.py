9941ef8717468784cd4579d39401d22e02b00e82
James Confidence <63427518+jamesconfy@users.noreply.github.com>
2022-10-27 14:42:16 +0100

    Updated routes

--- project1/routes.py
+++ project1/routes.py
@@ -1,11 +1,10 @@
-import json
-from flask import current_app as app, jsonify, make_response
+from flask import current_app as app, make_response
 
 @app.route("/")
 @app.route("/home")
 def home():
     res = { 
-            "slackUsername": "James Confidence", 
+            "slackUsername": "everybody", 
             "backend": True, 
             "age": 24, 
             "bio": "I am a self-taught programmer with experience working with Python, Flask, React, Docker and knowledge with hosting on AWS and Google Cloud Platform. I am a motivated person that picks up technologies easily"
